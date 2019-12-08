dec9e684d5e32e3380b30eabed0833461bf85712
Carsten Fuchs <carsten.fuchs@cafu.de>
2019-12-08 11:29:23 +0100

    Update settings according to Django 3.0

--- FlyMe/settings.py
+++ FlyMe/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'de-de'
+LANGUAGE_CODE = 'de'
 
 TIME_ZONE = 'Europe/Berlin'
 
