0baeb6bd088c7eb3adce5e7d0dc6f8a5d673788a
Carsten Fuchs <carsten.fuchs@cafu.de>
2020-01-05 11:48:33 +0100

    Add a template for rendering simple forms with Bootstrap 4

--- FlyMe/settings.py
+++ FlyMe/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'Accounts.apps.AccountsConfig',
     'Aviation.apps.AviationConfig',
     'Organizations.apps.OrganizationsConfig',
