d245a5cfbbd1743dc8442c7f8aeaaf987b95bfc8
Carsten Fuchs <carsten.fuchs@cafu.de>
2020-01-05 12:00:32 +0100

    Add views for user login, logout and password change
    This change adds URLs for Django's authentication views:
    
        /user/login/
        /user/logout/
        /user/password-change/
        /user/password-change-done/
    
    Related links are added to the navbar in the base template.

---
+++ Accounts/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='Accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='Accounts/logout.html'), name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='Accounts/password_change.html'), name='password-change'),
+    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(template_name='Accounts/password_change_done.html'), name='password-change-done'),
+]
--- FlyMe/urls.py
+++ FlyMe/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', include('Accounts.urls')),
     path('', include('Organizations.urls')),
 ]
