a55ae4bc3e1e7754dcd06ace1e8900b3ec432eb3
Carsten Fuchs <carsten.fuchs@cafu.de>
2020-01-08 10:43:09 +0100

    Add content to user overview page

--- Organizations/views/user_overview.py
+++ Organizations/views/user_overview.py
@@ -1,14 +1,20 @@
-from datetime import date
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
+from django.db.models import Q
 from django.shortcuts import render
+
+from Organizations.models import Membership, Ability
+from Aviation.models import Flight, Reservation
 
 
 @login_required
 @transaction.non_atomic_requests
 def view(request):
-    heute = date.today()
+    u = request.user
 
     return render(request, "Organizations/user_overview.html", {
-        "heute": heute,
+        "flights": Flight.objects.filter(Q(pic=u) | Q(co=u)),
+        "abilities": Ability.objects.filter(user=u),
+        "reservations": Reservation.objects.filter(user=u),
+        "memberships": Membership.objects.filter(user=u),
     })
