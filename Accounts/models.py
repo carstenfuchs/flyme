4e46d52aacc2dc111dae335d3c862c10218979b5
Carsten Fuchs <carsten.fuchs@cafu.de>
2020-01-10 14:04:40 +0100

    Enhance the main navbar
      - add link to Admin Site
      - add icons from Font Awesome (only one for the Admin at this time)
      - add Gravatar image for the logged in user
      - add link to the user's profile settings (still disabled)
      - rearrange some menu items from right to left

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from improved_user.model_mixins import AbstractUser
 
 
@@ -6,5 +7,10 @@ class User(AbstractUser):
     A custom user model based on package `django-improved-user`.
     See https://django-improved-user.readthedocs.io/ for details.
     """
+    def get_gravatar_url(self):
+        """Returns the URL of this user's Gravatar image."""
+        hash_ = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return f"https://www.gravatar.com/avatar/{hash_}"
+
     def __str__(self):
         return self.full_name
