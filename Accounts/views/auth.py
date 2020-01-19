d6f0920cd2176346c281713f9d9a850875e95c46
Carsten Fuchs <carsten.fuchs@cafu.de>
2020-01-19 15:00:54 +0100

    Make Accounts.views.auth independent
    Generally, while Accounts is made so that other apps can depend on it,
    Accounts itself must not depend on any other app that is "above" it in the
    dependency hierarchy.
    
    Here, specifically we had a reference to app Organizations left that has
    now been removed (introducing parameter `default_next`).

--- Accounts/views/auth.py
+++ Accounts/views/auth.py
@@ -1,42 +1,52 @@
-from django.contrib.auth import forms as auth_forms
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.password_validation import password_validators_help_texts
-from django.urls import reverse, reverse_lazy
+from django.conf import settings
+from django.contrib.auth import forms, password_validation, views
+from django.shortcuts import resolve_url
 from django.utils.safestring import mark_safe
 
 
-class LoginForm(auth_forms.AuthenticationForm):
+class LoginForm(forms.AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Keinen Doppelpunkt in Labels verwenden ("Benutzername", nicht "Benutzername:").
+
+        # Don't append colons to form labels ("Benutzername", nicht "Benutzername:").
         self.label_suffix = ""
 
 
-class LoginView(auth_views.LoginView):
+class LoginView(views.LoginView):
     authentication_form = LoginForm
+    default_next = None
 
     def get_success_url(self):
         """
-        Wenn kein `next` Parameter vorhanden ist, leite nicht nach
-        `settings.LOGIN_REDIRECT_URL` weiter (der gemeinsame Wert für alle Login Views),
-        sondern nach `ma-service:übersicht`.
+        If no `next` parameter is given, the login view normally redirects to
+        `settings.LOGIN_REDIRECT_URL`. This doesn't work well though if we have
+        several apps that each provide a login view. Thus, allow URL configs to
+        provide a custom default for `next`.
         """
         url = self.get_redirect_url()
-        return url or reverse('organizations:user-overview')
+        return url or resolve_url(self.default_next or settings.LOGIN_REDIRECT_URL)
 
 
-class PCForm(auth_forms.PasswordChangeForm):
+class LogoutView(views.LogoutView):
+    pass
+
+
+class PasswordChangeForm(forms.PasswordChangeForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # Keinen Doppelpunkt in Labels verwenden ("Benutzername", nicht "Benutzername:").
+        # Don't append colons to form labels ("Benutzername", nicht "Benutzername:").
         self.label_suffix = ""
 
-        # Setze vernünftige Hilfetexte ein (https://code.djangoproject.com/ticket/31158).
-        self.fields['new_password1'].help_text = mark_safe("<br>".join(password_validators_help_texts()))
-        self.fields['new_password2'].help_text = "Die doppelte Eingabe soll helfen, Tippfehler zu vermeiden."
+        # Set proper help texts (https://code.djangoproject.com/ticket/31158).
+        self.fields['new_password1'].help_text = mark_safe("<br>".join(password_validation.password_validators_help_texts()))
+        self.fields['new_password2'].help_text = "As you cannot see the password as you type, this helps catching typos."
+      # self.fields['new_password2'].help_text = "Die doppelte Eingabe soll helfen, Tippfehler zu vermeiden."
 
 
-class PasswordChangeView(auth_views.PasswordChangeView):
-    form_class = PCForm
-    success_url = reverse_lazy('organizations:password-change-done')
+class PasswordChangeView(views.PasswordChangeView):
+    form_class = PasswordChangeForm
+
+
+class PasswordChangeDoneView(views.PasswordChangeDoneView):
+    pass
--- Organizations/urls.py
+++ Organizations/urls.py
@@ -1,5 +1,4 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
 from Accounts.views import auth
 from .views import welcome, user_overview
 
@@ -7,10 +6,10 @@ from .views import welcome, user_overview
 app_name = 'organizations'
 
 urlpatterns = [
-    path('login/', auth.LoginView.as_view(template_name='Organizations/auth_login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='Organizations/auth_logout.html'), name='logout'),
-    path('password-change/', auth.PasswordChangeView.as_view(template_name='Organizations/auth_password_change.html'), name='password-change'),
-    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(template_name='Organizations/auth_password_change_done.html'), name='password-change-done'),
+    path('login/', auth.LoginView.as_view(template_name='Organizations/auth_login.html', default_next='organizations:user-overview'), name='login'),
+    path('logout/', auth.LogoutView.as_view(template_name='Organizations/auth_logout.html'), name='logout'),
+    path('password-change/', auth.PasswordChangeView.as_view(template_name='Organizations/auth_password_change.html', success_url=reverse_lazy('organizations:password-change-done')), name='password-change'),
+    path('password-change-done/', auth.PasswordChangeDoneView.as_view(template_name='Organizations/auth_password_change_done.html'), name='password-change-done'),
     path('', welcome.view, name='welcome'),
     path('user/overview/', user_overview.view, name='user-overview'),
 ]
