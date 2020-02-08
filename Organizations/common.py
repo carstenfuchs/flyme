992457a2eba599c254fab98dcc4d5a57dd36a72f
Carsten Fuchs <carsten.fuchs@cafu.de>
2020-02-08 18:06:44 +0100

    Renames only
    The new names are clearer and less ambiguous.

--- Organizations/common.py
+++ Organizations/common.py
@@ -2,10 +2,10 @@ from django.shortcuts import get_object_or_404
 from Accounts.models import User
 
 
-def get_relevant_user_or_404(request, user_id):
-    if user_id is None or user_id == request.user.id:
-        return request.user
+def get_managed_user(managing_user, other_user_id):
+    if other_user_id is None or other_user_id == managing_user.id:
+        return managing_user
 
     # TODO: Is request.user allowed to access the user with `user_id`?
 
-    return get_object_or_404(User, id=user_id)
+    return get_object_or_404(User, id=other_user_id)
--- Organizations/views/abilities.py
+++ Organizations/views/abilities.py
@@ -3,18 +3,18 @@ from django.db import transaction
 from django.db.models import F
 from django.shortcuts import render
 
-from Organizations.common import get_relevant_user_or_404
+from Organizations.common import get_managed_user
 from Organizations.models import Ability
 
 
 @login_required
 @transaction.non_atomic_requests
 def list_view(request, user_id=None):
-    rel_user = get_relevant_user_or_404(request, user_id)
-    abilities = Ability.objects.filter(user=rel_user).order_by(F('expires').asc(nulls_last=True), 'kind')
+    mu = get_managed_user(request.user, user_id)
+    abilities = Ability.objects.filter(user=mu).order_by(F('expires').asc(nulls_last=True), 'kind')
 
     return render(
         request,
         'Organizations/ability_list.html',
-        {'u': rel_user, 'ability_list': abilities}
+        {'mu': mu, 'ability_list': abilities}
     )
