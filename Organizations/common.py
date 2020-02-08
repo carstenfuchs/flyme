60c8a85cf53181136f7fe95206163062dd15e2ad
Carsten Fuchs <carsten.fuchs@cafu.de>
2020-02-08 18:12:28 +0100

    Add checks: Is the logged in user permitted to manage the desired user?

--- Organizations/common.py
+++ Organizations/common.py
@@ -1,11 +1,49 @@
-from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
 from Accounts.models import User
 
 
+def get_all_managed_users_QS(managing_user):
+    """
+    Returns a QuerySet of all users that the given user can manage (possibly
+    not including himself).
+    """
+
+    # Below we would like to write
+    #     membership__orga__membership__status__ne="x",
+    # but it is still unclear if this is equivalent to
+    #     ~Q(membership__orga__membership__status="x"),
+    # Comparing the generated SQL might suffice to settle this?
+    # See:
+    #   - https://code.djangoproject.com/ticket/5763#comment:14
+    #   - https://groups.google.com/d/msg/django-developers/T8XhqQmFrig/dxb2vjuchlQJ
+
+    return User.objects.filter(
+        membership__orga__membership__user=managing_user,
+        membership__orga__membership__begin__lte=heute,
+        membership__orga__membership__ende__gte=heute,
+        membership__orga__membership__status__in=["a", "p", "e", "o"],  # see comment above
+        membership__orga__membership__is_manager=True,
+    )
+
+
 def get_managed_user(managing_user, other_user_id):
+    """
+    If `managing_user` is permitted to manage the user with `other_user_id`,
+    the related user instance is returned. Raises `PermissionDenied` otherwise.
+    """
     if other_user_id is None or other_user_id == managing_user.id:
         return managing_user
 
-    # TODO: Is request.user allowed to access the user with `user_id`?
-
-    return get_object_or_404(User, id=other_user_id)
+    # Is `managing_user` allowed to access the user with `other_user_id`?
+    # Yes, if he today has an active membership with management permission
+    # in an organization in which the other user is a member as well.
+    # Note that access is *not* allowed in this situation:
+    #
+    # managing_user --- managing member -- orga 1  (other user is not a member)
+    #                \
+    #                 -- non-managing member -- orga 2 -- other user is a member
+    #
+    try:
+        return get_all_managed_users_QS(managing_user).get(id=other_user_id)
+    except User.DoesNotExist:
+        raise PermissionDenied
